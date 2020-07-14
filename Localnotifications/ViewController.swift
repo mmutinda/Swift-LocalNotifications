3a4e56dac38d235f7e671b6eedb2026af0ce4c64
Michael Mutinda <mutindamike@gmail.com>
2020-07-14 08:20:36 +0300

    Initial Commit

---
+++ Localnotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localnotifications
+//
+//  Created by Michael Mutinda on 7/14/20.
+//  Copyright © 2020 Michael Mutinda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
